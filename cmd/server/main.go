6b31c1f72c42a22da8d8f4ea4b7717e8d2d79f78
spike <sebfrye@gmail.com>
2026-03-18 20:22:30 -0500

    Moved the logic to create and start the server to it's own package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
-
-	"github.com/a-h/templ"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/spoik/go-htmx-todo/internal/templates"
 	"github.com/spoik/go-htmx-todo/internal/db"
+	"github.com/spoik/go-htmx-todo/internal/server"
 )
 
 func main() {
 	db := db.Connect()
 	defer db.Close()
 
-	mux := createServer()
-	startServer(mux, 8080)
-}
-
-func createServer() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.Handle("/", templ.Handler(templates.Hello("World")))
-	return mux
-}
-
-func startServer(mux *http.ServeMux, port int) {
-	log.Printf("Starting server on :%d\n", port)
-	err := http.ListenAndServe(
-		fmt.Sprintf(":%d", port),
-		mux,
-	)
-
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	server.Start(8080)
 }
---
+++ internal/server/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/spoik/go-htmx-todo/internal/templates"
+)
+
+func create() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", templ.Handler(templates.Hello("World")))
+
+	return mux
+}
+
+func Start(port int) {
+    mux := create()
+
+	log.Printf("Starting server on :%d\n", port)
+	err := http.ListenAndServe(
+		fmt.Sprintf(":%d", port),
+		mux,
+	)
+
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
