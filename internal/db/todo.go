0eeb95a7e2c825fe57ed39503b40ba349b975a8f
spike <sebfrye@gmail.com>
2026-03-27 10:18:02 -0500

    Moved todos to the db package. Changed the todos to be an in memory slice to act like a database.

---
+++ internal/db/todo.go
@@ -0,0 +1,8 @@
+package db
+
+type Todo struct {
+	ID       string
+	Title    string
+	Complete bool
+}
+
--- internal/server/server.go
+++ internal/server/server.go
@@ -6,19 +6,32 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/spoik/go-htmx-todo/internal/db"
 	"github.com/spoik/go-htmx-todo/internal/templates"
 )
+
+var todos = []db.Todo{
+	{
+		ID:    "1",
+		Title: "Todo 1",
+	},
+	{
+		ID:       "2",
+		Title:    "Todo 2",
+		Complete: true,
+	},
+}
 
 func create() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", templ.Handler(templates.Todos()))
+	mux.Handle("/", templ.Handler(templates.Todos(todos)))
 
 	return mux
 }
 
 func Start(port int) {
-    mux := create()
+	mux := create()
 
 	log.Printf("Starting server on :%d\n", port)
 	err := http.ListenAndServe(
