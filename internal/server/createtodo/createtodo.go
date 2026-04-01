d40b8cdf4064caa569e2117a9478ec405cf60a6f
spike <sebfrye@gmail.com>
2026-04-01 17:38:02 -0500

    Added create todo todo tasks

--- internal/server/createtodo/createtodo.go
+++ internal/server/createtodo/createtodo.go
@@ -3,5 +3,7 @@ package createtodo
 import "net/http"
 
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
+	// TODO: Validate new todo
+	// TODO: Try to write the new todo to the database
 	w.Write([]byte("todo created"))
 }
