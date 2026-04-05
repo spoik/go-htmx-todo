72fff834c915a8f1d0faf7b50f5675d5be00572c
Spike Frye <sebfrye@gmail.com>
2026-04-04 22:15:43 -0500

    Refactor: remove unused queries field from Server struct
    - Remove the unused 'queries' field from the Server struct.
    - Reorder internal server imports for consistency.

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,15 +8,14 @@ import (
 	"github.com/spoik/go-htmx-todo/internal/database/queries"
 	"github.com/spoik/go-htmx-todo/internal/server/createtodo"
 	"github.com/spoik/go-htmx-todo/internal/server/listtodos"
-	"github.com/spoik/go-htmx-todo/internal/server/updatetodocomplete"
 	"github.com/spoik/go-htmx-todo/internal/server/middleware"
 	"github.com/spoik/go-htmx-todo/internal/server/newtodo"
 	"github.com/spoik/go-htmx-todo/internal/server/routes"
+	"github.com/spoik/go-htmx-todo/internal/server/updatetodocomplete"
 )
 
 type Server struct {
 	handler *http.Handler
-	queries *queries.Queries
 }
 
 func New(q *queries.Queries) *Server {
@@ -29,10 +28,7 @@ func New(q *queries.Queries) *Server {
 
 	wrappedMux := middleware.LogRequests(mux)
 
-	return &Server{
-		handler: &wrappedMux,
-		queries: q,
-	}
+	return &Server{handler: &wrappedMux}
 }
 
 func (s *Server) Start(port int) {
